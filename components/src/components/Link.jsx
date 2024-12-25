d3690aaf9743f79c66fa3e758b304eb92f9df104
Premanand Chowdhury <freudigprem@gmail.com>
2024-12-25 18:23:42 +0000

    choe: open the page in new tab behaviour

--- components/src/components/Link.jsx
+++ components/src/components/Link.jsx
@@ -6,6 +6,10 @@ const Link = ({ to, children }) => {
   const { navigate } = useContext(NavigationContext);
 
   const handleClick = (e) => {
+    // If the user is holding down the command key (mac) or control key (windows), don't do anything
+    if (e.metaKey || e.ctrlKey) {
+      return;
+    }
     e.preventDefault();
     navigate(to);
   };
